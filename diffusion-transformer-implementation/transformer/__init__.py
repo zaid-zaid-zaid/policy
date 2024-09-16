9650067c6a080eae2bfbcd27872b91ea6dcc66c6
Zaid Ahmad <zaidahmad@berkeley.edu>
2024-09-16 00:13:34 -0400

    add dit implementation

---
+++ diffusion-transformer-implementation/transformer/__init__.py
@@ -0,0 +1,9 @@
+from transformer.denoising_diffusion import Denoising
+from transformer.nn.dit import DiT
+from transformer.parameterization import Parameterization
+
+__all__ = [
+    "Denoising",
+    "DiT",
+    "Parameterization",
+]
